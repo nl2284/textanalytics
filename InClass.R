04cd40714200265cd40bcc83ee46e357829ea345
nl2284 <nl2284@tc.columbia.edu>
2017-11-01 19:59:40 -0700

    updated inclass.R

--- InClass.R
+++ InClass.R
@@ -31,7 +31,31 @@ str_sub("testing", start=4,
         end=str_length("testing"))
 str_sub("testing", start=1, end=4)
 
+
 #6
 input<-c("abc", "defg")
 str_sub(input, c(2, 3)) ##start number vector
+
+email<-readRDS("email.rds")
+cat(email[8])
+
+
+##7 Use str_locate() to identify the locaiton of the blank line
+
+str_locate(string=email[1], pattern="\n\n")
+
+#
+metadata1<-str_sub(string=email[1], start=1, end=842)
+body1<-str_sub(string=email[1], start=846)
+cat(metadata1)
+cat(body1)
+
+str_split(string=metadata1, pattern="\n")
+
+loc<-str_locate(string=email, pattern="\n\n")
+
+metadata<-str_sub(string=email, start=rep(1, length(email), end=loc[,1]) )
+body<-str_sub(string=email, start=loc[,2])
+
+
 
